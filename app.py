f764db15c570ff8f19d9bcc19ba90f5ced34a041
Bruno Miglioretto <bruno.miglioretto@gmail.com>
2023-03-17 21:03:22 -0300

    use template.html

--- app.py
+++ app.py
@@ -1,30 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from faker import Faker
 
 app = Flask(__name__)
+
+faker = Faker()
+
+links = [
+    ["/orders", "Orders"],
+    ["/clients", "Clients"],
+    ["/employees", "Employees"],
+]
+orders_mock = [
+    "Salad",
+    "Some fruit",
+    "Some garbage",
+    "Some fastfood",
+    "Whatever",
+]
+
+clients_mock = [faker.name() for _ in range(5)]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", links=links)
 
 
 @app.route("/orders")
 def orders():
     return render_template(
         "orders.html",
-        orders=[
-            "Salad",
-            "Some fruit",
-            "Some garbage",
-            "Some fastfood",
-            "Whatever",
-        ],
+        links=links,
+        orders=orders_mock,
     )
+
+@app.route("/test")
+def test():
+    return url_for("orders")
 
 
 @app.route("/clients")
 def clients():
-    return render_template("clients.html")
+    return render_template("clients.html", links=links, clients=clients_mock)
 
 
 @app.route("/employees")
